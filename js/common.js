2f6b1a57c742bc512bdb44119454139950906093
alshchetinin <alexsander.qw@gmail.com>
2017-05-27 09:58:38 +0500

    Проверка знака в ЮРЛ

--- js/common.js
+++ js/common.js
@@ -1,8 +1,52 @@
-$(window).load(function(){ // вешаем скрипт на load ибо при ready chrome криво достаёт размеры
-var hh = $('#head').height(); // берем высоту шапки и суем в переменную hh
-var fh = $('footer').height(); // то же самое с подвалом
-var wh = $(window).height(); // высота всего окна
-var сh = wh - hh - fh; // считаем оптимальную высоту для блока с контентом
-$('#cont').css('min-height', сh); // применяем посчитанную высоту
-});
+var utmApp = angular.module('utmApp', [])
+ utmApp.controller('utmAddController', ['$scope', function($scope) {
+
+    $scope.utmAddSearchYandex = function() { //Кнопка отвечает за постановку ЮТМки для Яндекс:Поиск
+    	var imputData = $(".input_href").val().indexOf('?') + 1; //Проверяет содержится ли ?
+    	if (imputData > 0) {
+  			$scope.utm = '&utm_source=yandex.search&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={ad_id}&utm_term={keyword}';
+    	}
+			else {
+  			$scope.utm = '?utm_source=yandex.search&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={ad_id}&utm_term={keyword}';
+			};
+    };
+
+    $scope.utmAddContextYandex = function() { //Кнопка отвечает за постановку ЮТМки для Яндекс:РСЯ
+    	var imputData = $(".input_href").val().indexOf('?') + 1; //Проверяет содержится ли ?
+    	if (imputData > 0) {
+  			$scope.utm ='&utm_source=yandex.context&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={ad_id}&utm_term={keyword}';
+    	}
+			else {
+  			$scope.utm = '?utm_source=yandex.context&utm_medium=cpc&utm_campaign={campaign_id}&utm_content={ad_id}&utm_term={keyword}';
+			};
+    };
+
+    $scope.utmAddSearchGoogle = function() { //Кнопка отвечает за постановку ЮТМки для Гугл:Поиск
+    	var imputData = $(".input_href").val().indexOf('?') + 1; //Проверяет содержится ли ?
+    	if (imputData > 0) {
+  			$scope.utm ='&utm_source=google.search&utm_medium=cpc&utm_campaign={campaignid}&utm_content={creative}&utm_term={keyword}';
+    	}
+			else {
+  			$scope.utm = '?utm_source=google.search&utm_medium=cpc&utm_campaign={campaignid}&utm_content={creative}&utm_term={keyword}';
+			};
+    };
+
+
+    $scope.utmAddContextGoogle = function() { //Кнопка отвечает за постановку ЮТМки для Гугл:КМС
+    	var imputData = $(".input_href").val().indexOf('?') + 1; //Проверяет содержится ли ?
+    	if (imputData > 0) {
+  			$scope.utm ='&utm_source=google.kms&utm_medium=cpc&utm_campaign={campaignid}&utm_content={creative}&placement={placement}&utm_term={keyword}';
+    	}
+			else {
+  			$scope.utm = '?utm_source=google.kms&utm_medium=cpc&utm_campaign={campaignid}&utm_content={creative}&placement={placement}&utm_term={keyword}';
+			};
+    };
+
+
+}]);
+
+
+
+
+
 
