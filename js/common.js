20e876dee017716447e7fefeb651de9f83c7a3dd
alshchetinin <alexsander.qw@gmail.com>
2017-05-28 14:48:46 +0500

    345

--- js/common.js
+++ js/common.js
@@ -57,11 +57,17 @@ var utmApp = angular.module('utmApp', [])
 
 }]);
 
- $('#vk.cpc').click(function() {
-	if ($('.input_campain').is(':visible')) {
-		$('.input_campain').hide('slow');
-	}
+ $(function () {                                      // Когда страница загрузится
+    $('.container a').each(function () {             // получаем все нужные нам ссылки
+        var location = window.location.href; // получаем адрес страницы
+        var link = this.href;                // получаем адрес ссылки
+        if(location == link) {               // при совпадении адреса ссылки и адреса окна
+            $(this).addClass('active');  //добавляем класс
+        }
+    });
 });
+
+
 
 
 
