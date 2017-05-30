d581b19f75adce7f93cf94cdf7a87fd2ab59f097
alshchetinin <alexsander.qw@gmail.com>
2017-05-30 16:13:14 +0500

    Убрал высоту

--- js/common.js
+++ js/common.js
@@ -97,7 +97,7 @@ var utmApp = angular.module('utmApp', [])
 }]);
 
 $(function () {                                      // Когда страница загрузится
-    $('.container a').each(function () {             // получаем все нужные нам ссылки
+    $('#menu a').each(function () {             // получаем все нужные нам ссылки
         var location = window.location.href; // получаем адрес страницы
         var link = this.href;                // получаем адрес ссылки
         if(location == link) {               // при совпадении адреса ссылки и адреса окна
